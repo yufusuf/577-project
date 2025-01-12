e0ba3a4b6b5579c306ade3f4c4936cddc0ad6fad
yufusuf <yusufsahin429@gmail.com>
2025-01-13 02:18:46 +0300

    plot up

--- plot_generator.py
+++ plot_generator.py
@@ -17,7 +17,7 @@ with open(file_path, "r") as f:
 # Sort results by the 'size' field (last column)
 results.sort(key=lambda x: x[8])
 
-# Separate the results into individual lists
+# Separate the results into individual lists and multiply times by 1000 (convert to ms)
 dgtsv_time = [r[0] * 1000 for r in results]
 dgtsv_err = [r[1] for r in results]  # Read error but don't use it yet
 dgesv_time = [r[2] * 1000 for r in results]
@@ -27,6 +27,10 @@ cyclic_seq_err = [r[5] for r in results]  # Read error but don't use it yet
 cyclic_par_time = [r[6] * 1000 for r in results]
 cyclic_par_err = [r[7] for r in results]  # Read error but don't use it yet
 sizes = [r[8] for r in results]
+
+# Filter out entries with DGESV time of 0
+filtered_sizes = [s for s, t in zip(sizes, dgesv_time) if t != 0]
+filtered_dgesv_time = [t for t in dgesv_time if t != 0]
 
 # Define consistent colors
 colors = {
@@ -38,7 +42,7 @@ colors = {
 
 # Save execution times to file
 with open("./results/execution_times.txt", "w") as out_file:
-    out_file.write("Size,DGTSV Time,DGESV Time,Cyclic Sequential Time,Cyclic Parallel Time\n")
+    out_file.write("Size,DGTSV Time (ms),DGESV Time (ms),Cyclic Sequential Time (ms),Cyclic Parallel Time (ms)\n")
     for size, dgtsv, dgesv, cyclic_seq, cyclic_par in zip(sizes, dgtsv_time, dgesv_time, cyclic_seq_time, cyclic_par_time):
         out_file.write(f"{size},{dgtsv},{dgesv},{cyclic_seq},{cyclic_par}\n")
 
@@ -57,7 +61,7 @@ plt.close()
 # Plot execution times (all)
 plt.figure(figsize=(12, 6))
 plt.plot(sizes, dgtsv_time, label="DGTSV Time", marker="o", color=colors["dgtsv"])
-plt.plot(sizes, dgesv_time, label="DGESV Time", marker="o", color=colors["dgesv"])
+plt.plot(filtered_sizes, filtered_dgesv_time, label="DGESV Time", marker="o", color=colors["dgesv"])
 plt.plot(sizes, cyclic_seq_time, label="Cyclic Sequential Time", marker="o", color=colors["cyclic_seq"])
 plt.plot(sizes, cyclic_par_time, label="Cyclic Parallel Time", marker="o", color=colors["cyclic_par"])
 plt.xlabel("Matrix Size")
